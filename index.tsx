1291e74e246120b57e0ce9d56d2b102d21838443
r3phjacobs-dev <r3ph.jacobs@gmail.com>
2026-01-31 01:39:10 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,22 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (!rootElement) {
+  console.error("Gagal menemukan elemen root!");
+} else {
+  try {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  } catch (err) {
+    console.error("React Render Error:", err);
+    rootElement.innerHTML = `<div style="padding: 20px; color: red;">Gagal memuat aplikasi. Silakan muat ulang.</div>`;
+  }
+}
